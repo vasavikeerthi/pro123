40a40337ac7c3886c1dc8ce3925f92757f8e1b74
vasavikeerthi <kannavasavikeerthi@gmail.com>
2020-10-06 09:26:27 -0400

    vk

---
+++ Helloworld/src/com/vk/User.java
@@ -0,0 +1,10 @@
+package com.vk;
+
+public class User {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("Hello Welcome to java world");
+	}
+
+}
