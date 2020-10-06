20ad95692f4f55add9bb7b6d449a357c2f6c3495
vasavikeerthi <kannavasavikeerthi@gmail.com>
2020-10-06 18:33:47 -0400

    keerthi's commit

--- Helloworld/src/com/vk/User.java
+++ Helloworld/src/com/vk/User.java
@@ -1,7 +1,7 @@
 package com.vk;
 
 public class User {
-
+// my new learning of github
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("Hello Welcome to java world");
